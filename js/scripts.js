5f19382435842cac30cac41ad67661e4dd61512f
Rysth <johnpalacios.t@gmail.com>
2023-07-08 10:17:02 -0500

    Update scripts.js to add each Category dynamically.

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,34 @@
 const brandSystem = document.querySelector('#brand-system');
 
 class Image {
-  constructor(className, src, alt) {
+  constructor(id, className, src, alt) {
+    this.id = id;
     this.className = className;
     this.src = src;
     this.alt = alt;
   }
 }
 
-const brandImage = [
-  new Image('', './resources/images/SVG/brands/brand-1.svg', 'Lacoste Brand Logo'),
-  new Image('', './resources/images/SVG/brands/brand-2.svg', 'Adidas Brand Logo'),
-  new Image('', './resources/images/SVG/brands/brand-3.svg', 'Amazon Brand Logo'),
-  new Image('', './resources/images/SVG/brands/brand-4.svg', 'Levis Brand Logo'),
-  new Image('', './resources/images/SVG/brands/brand-5.svg', 'Shopify Brand Logo'),
+const brandImages = [
+  new Image(
+    0,
+    '',
+    './resources/images/SVG/brands/brand-1.svg',
+    'Lacoste Brand Logo'
+  ),
+  new Image(1, '', './resources/images/SVG/brands/brand-2.svg', 'Adidas Brand Logo'),
+  new Image(2, '', './resources/images/SVG/brands/brand-3.svg', 'Amazon Brand Logo'),
+  new Image(3, '', './resources/images/SVG/brands/brand-4.svg', 'Levis Brand Logo'),
+  new Image(
+    4,
+    '',
+    './resources/images/SVG/brands/brand-5.svg',
+    'Shopify Brand Logo'
+  ),
 ];
 
-function chargeBrandImage() {
-  brandSystem.innerHTML = brandImage
+function loadBrandImages() {
+  brandSystem.innerHTML = brandImages
     .map((element, index) => {
       return `
     <img
@@ -46,4 +57,80 @@ function chargeBrandImage() {
   }).mount();
 }
 
-window.onload = chargeBrandImage;
+loadBrandImages();
+
+// Creates Dynamically the Category System to show each Image
+const categorySystem = document.querySelector('#category-system');
+
+class Category {
+  constructor(id, className, url, image, subtitle, description) {
+    this.id = id;
+    this.className = className;
+    this.url = url;
+    this.image = image;
+    this.subtitle = subtitle;
+    this.description = description;
+  }
+}
+
+const categoryImages = [
+  new Image(
+    0,
+    '',
+    './resources/images/PNG/category/category-1.png',
+    'Category Number 1'
+  ),
+  new Image(
+    1,
+    '',
+    './resources/images/PNG/category/category-2.png',
+    'Category Number 2'
+  ),
+  new Image(
+    2,
+    '',
+    './resources/images/PNG/category/category-3.png',
+    'Category Number 3'
+  ),
+];
+const categoryItems = [
+  new Category(
+    0,
+    '',
+    '#',
+    categoryImages[0],
+    'Hoodies & Sweetshirt',
+    'Explore Now!'
+  ),
+  new Category(1, '', '#', categoryImages[1], 'Coats & Parkas', 'Explore Now!'),
+  new Category(2, '', '#', categoryImages[2], 'Tees & T-Shirt', 'Explore Now!'),
+];
+
+function loadCategoryItems() {
+  categorySystem.innerHTML = categoryItems
+    .map((element, index) => {
+      return `
+     <div class="category-card">
+        <a class="category-link" href="${element.className}">
+          <picture class="category-picture">
+            <img
+              class="category-image"
+              src="${element.image.src}"
+              alt="Category Image 1"
+            />
+          </picture>
+          <div class="category-content">
+            <div class="category-name">
+              <h3 class="category-subtitle">${element.subtitle}</h3>
+              <p class="category-description">${element.description}</p>
+            </div>
+            <i class="category-arrow fas fa-arrow-right"></i>
+          </div>
+        </a>
+      </div>
+    `;
+    })
+    .join('');
+}
+
+loadCategoryItems();
